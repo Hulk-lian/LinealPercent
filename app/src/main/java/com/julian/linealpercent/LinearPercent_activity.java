0fd161adc9f6efd431ff78d203db03b32d47c3fa
Julián Troyano <juliantrodom@gmail.com>
2016-10-17 12:46:10 +0200

    commit 1

---
+++ app/src/main/java/com/julian/linealpercent/LinearPercent_activity.java
@@ -0,0 +1,13 @@
+package com.julian.linealpercent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearPercent_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_percent_activity);
+    }
+}
